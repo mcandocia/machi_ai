3eed0e150dcbd108688e1d2b5720593f9479b359
Max Candocia <mscandocia@gmail.com>
2017-07-20 23:04:31 -0500

    more interesting output

--- machikoro.py
+++ machikoro.py
@@ -6,7 +6,7 @@ from game import Game
 from player import Player
 from player_ai import SharedAI
 
-total_turns = []
+
 
 def main(load):
 	game = Game(0)
@@ -15,21 +15,27 @@ def main(load):
 		players[0].load_ai()
 	shared_ai = SharedAI(players)
 	game.run() 
+	total_turns = []
 	for k in range(25):
+		current_cycle = []
 		print '---k=%d---' % k
 		for j in range(10):
 			print 'in training round j=%d' % j
-			for i in range(10):
+			for i in range(50):
 				new_game = Game(i, players)
 				new_game.run()
-				total_turns.append(new_game.turn)
+				current_cycle.append(new_game.turn)
 			print 'training models'
 			new_game.train_players()
 		new_game.flush_player_history()
 		players[0].save_ai()
+		total_turns.append(current_cycle)
+		print 'cycle #%d had mean turns of %.01f' % (k, float(sum(current_cycle))/500.)
 		print 'flushed history'
+	means = [float(sum(x))/500 for x in total_turns]
+	for i in range(25):
+		print 'cycle #%d mean: %.01f' % (i, means[i])
 	print 'done!'
-	print turn 
 
 if __name__=='__main__':
 	if len(sys.argv) > 1:
